167d68c8bd5dec0de14ab248ad042247cad3f911
marcinbodnardesktop <me@marcinbodnar.com>
2020-10-27 19:34:55 +0100

    Alerts: new alert for Send Money

--- src/actions/account.js
+++ src/actions/account.js
@@ -395,7 +395,7 @@ export const { signAndSendTransactions, setSignTransactionStatus, sendMoney } = 
     ],
     SEND_MONEY: [
         wallet.sendMoney.bind(wallet),
-        () => defaultCodesFor('account.sendMoney', { onlyError: true })
+        () => showAlert({ onlyError: true })
     ]
 })
 
--- src/reducers/account/index.js
+++ src/reducers/account/index.js
@@ -54,7 +54,7 @@ const loaderReducer = (state, { type, ready }) => {
 const globalAlertReducer = handleActions({
     // TODO: Reset state before action somehow. On navigate / start of other action?
     // TODO: Make this generic to avoid listing actions
-    [combineActions(addAccessKey, addAccessKeySeedPhrase, setupRecoveryMessage, saveAndSelectLedgerAccounts, deleteRecoveryMethod, deployMultisig, sendMoney, removeAccessKey, signAndSendTransactions, addLedgerAccessKey)]: (state, { error, ready, payload, meta }) => ({
+    [combineActions(addAccessKey, addAccessKeySeedPhrase, setupRecoveryMessage, saveAndSelectLedgerAccounts, deleteRecoveryMethod, deployMultisig, removeAccessKey, signAndSendTransactions, addLedgerAccessKey)]: (state, { error, ready, payload, meta }) => ({
         ...state,
         globalAlert: ready ? {
             success: !error,
